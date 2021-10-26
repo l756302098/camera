3481372e6448dff6f8598a7630947f82af299ae2
li <1976954875@qq.com>
2021-10-26 10:51:49 +0800

    update

--- fixed_hk_nosdk/scripts/rtsp_node.py
+++ fixed_hk_nosdk/scripts/rtsp_node.py
@@ -21,10 +21,11 @@ if __name__ == '__main__':
         device_password = rospy.get_param("~device_password","123qweasd")
         image_width = rospy.get_param("~image_width",1920)
         image_height = rospy.get_param("~image_height",1080)
+        channel = rospy.get_param("~channel",1)
         xml_path = rospy.get_param("~xml_path","")
         pixel_bytes = rospy.get_param("~pixel_bytes",4)
         use_ntp_time = rospy.get_param("~use_ntp_time",False)
-        print(device_ip,device_port,device_username,device_password,xml_path)
+        print(device_ip,device_port,device_username,device_password,xml_path,channel)
         config_dict["server_ip"] = device_ip
         config_dict["server_port"] = device_port
         config_dict["server_username"] = device_username
@@ -33,23 +34,24 @@ if __name__ == '__main__':
         print("start check temp data lenght")
         check = True
         while(check):
-            length,ok = api.get_pixelToPixelParam(device_ip,1,device_username,device_password)
+            length,ok = api.get_pixelToPixelParam(device_ip,channel,device_username,device_password)
             if ok :
                 if length == str(pixel_bytes):
+                    print("bytes length ",length)
                     check = False
                 else:
-                    ok = api.put_pixelToPixelParam(xml_path,device_ip,pixel_bytes,1,device_username,device_password)
+                    ok = api.put_pixelToPixelParam(xml_path,device_ip,pixel_bytes,channel,device_username,device_password)
             sleep(1)
         print("end check temp data lenght")
         print("start check video type")
         check = True
         while(check):
-            video_type,ok = api.get_streamParam(device_ip,1,device_username,device_password)
+            video_type,ok = api.get_streamParam(device_ip,channel,device_username,device_password)
             if ok :
                 if video_type == 'pixel-to-pixel_thermometry_data':
                     check = False
                 else:
-                    ok = api.put_streamParam(device_ip,1,device_username,device_password)
+                    ok = api.put_streamParam(device_ip,channel,device_username,device_password)
             sleep(1)
         print("finish check video type")
         #init rtsp client
@@ -71,30 +73,30 @@ if __name__ == '__main__':
                 time_array = data[0:16]
                 # timeStr = ":".join("{:02x}".format(c) for c in time_array)
                 # print("timeStr",timeStr)
-                # years/mouth/week/day/hour/minutes/second/mils
-                years = (time_array[0] << 8) + time_array[1]
-                mouth = (time_array[2] << 8) + time_array[3]
-                week = (time_array[4] << 8) + time_array[5]
-                day = (time_array[6] << 8) + time_array[7]
-                hour = (time_array[8] << 8) + time_array[9]
-                minutes = (time_array[10] << 8) + time_array[11]
-                second = (time_array[12] << 8) + time_array[13]
-                mils = (time_array[14] << 8) + time_array[15]
-                timestampFormat = "{:0>4d}-{:0>2d}-{:0>2d} {:0>2d}:{:0>2d}:{:0>2d}".format(years,mouth,day,hour,minutes,second)
-                # print("timestamp format",timestampFormat)
-                timeArray = time.strptime(timestampFormat, "%Y-%m-%d %H:%M:%S")
-                timeStamp = int(time.mktime(timeArray))
                 # timeStamp = timeStamp*1000 + mils
                 # print("timeStamp:",timeStamp)
                 data_array = data[20:]
-                image_size = image_height * image_width * 2
+                image_size = image_height * image_width * pixel_bytes
                 if len(data_array)!=image_size:
-                    #print("error size:",len(data_array))
+                    print("error size:",len(data_array))
                     continue
-                # print("ok size:",len(data_array))
+                print("ok size:",len(data_array))
                 #publish temp data
                 image_temp=Image()
                 if use_ntp_time:
+                    # years/mouth/week/day/hour/minutes/second/mils
+                    years = (time_array[0] << 8) + time_array[1]
+                    mouth = (time_array[2] << 8) + time_array[3]
+                    week = (time_array[4] << 8) + time_array[5]
+                    day = (time_array[6] << 8) + time_array[7]
+                    hour = (time_array[8] << 8) + time_array[9]
+                    minutes = (time_array[10] << 8) + time_array[11]
+                    second = (time_array[12] << 8) + time_array[13]
+                    mils = (time_array[14] << 8) + time_array[15]
+                    timestampFormat = "{:0>4d}-{:0>2d}-{:0>2d} {:0>2d}:{:0>2d}:{:0>2d}".format(years,mouth,day,hour,minutes,second)
+                    # print("timestamp format",timestampFormat)
+                    timeArray = time.strptime(timestampFormat, "%Y-%m-%d %H:%M:%S")
+                    timeStamp = int(time.mktime(timeArray))
                     header = Header()
                     header.stamp.secs = timeStamp
                     header.stamp.nsecs = mils * 1000000
